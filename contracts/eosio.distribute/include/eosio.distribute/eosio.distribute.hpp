4ed14ac1a580585eb80bbf346894cf44feca9510
deck <deckb@@users.noreply.github.com>
2021-05-03 12:25:11 -0400

    add documentation

--- contracts/eosio.distribute/include/eosio.distribute/eosio.distribute.hpp
+++ contracts/eosio.distribute/include/eosio.distribute/eosio.distribute.hpp
@@ -25,7 +25,7 @@ namespace eosiodistribute {
     };
 
     struct [[eosio::table("distribstate") , eosio::contract("eosio.distrb")]] distrib_state {
-        std::vector<distribute_account>  accounts;
+        std::vector<distribute_account>  accounts; // list of claimant accounts and their percentage of the distribution
 
         EOSLIB_SERIALIZE (distrib_state, (accounts))
     };
@@ -33,14 +33,16 @@ namespace eosiodistribute {
     using distrib_state_singleton = eosio::singleton< "distribstate"_n, distrib_state >;
     
     struct [[eosio::table("distribclaim") , eosio::contract("eosio.distrb")]] distrbute_claimer {
-        name        account;
-        asset       balance;
+        name        account;    // account that can claim the distribution
+        asset       balance;    // current balance of the claimant account
 
         uint64_t    primary_key() const { return account.value; }
     };
     using claimer_table = eosio::multi_index<"claimers"_n, distrbute_claimer>;
+    
     /**
-     * 
+     * A reference contract for distributing received `core tokens` to accounts. Special consideration
+     * is given to the `eosio.rex` to distribute all tokens to REX participants. 
      **/
     class [[eosio::contract("eosio.distribute")]] distribute_contract : public contract {
         using contract::contract;
@@ -50,19 +52,29 @@ namespace eosiodistribute {
             ~distribute_contract() {} 
 
             /**
+             * Set the accounts and their percentage of the distributed tokens.
              * 
+             * @pre accounts exist
+             * @pre percentages add up to 100%
+             * 
+             * @post any tokens sent to the contract account will be distributed based on the percentage
+             * @post if `accounts` is an empty array no tokens will be distributed
              * */
             [[eosio::action]]
             void setdistrib(const std::vector<distribute_account>& accounts);
 
             /**
+             * Claim tokens that have been marked for distribution.
              * 
+             * @pre `claimer` has tokens to claim
+             * 
+             * @post row in `claimers` table will be erased
              * */
             [[eosio::action]]
             void claimdistrib(const name& claimer);
 
             /**
-             * 
+             * Action that will be called when eosio.token::transfer is performed.
              **/
             [[eosio::on_notify("eosio.token::transfer")]]
             void distribute(name from, name to, asset quantity, eosio::ignore<std::string> memo);
