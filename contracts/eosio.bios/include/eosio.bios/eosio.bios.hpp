6c6f74ac7b2328944838e43c1ed118ff57617184
Todd Fleming <tbfleming@gmail.com>
2022-02-08 19:34:35 -0500

    clsdk: eosio.bios

--- contracts/eosio.bios/include/eosio.bios/eosio.bios.hpp
+++ contracts/eosio.bios/include/eosio.bios/eosio.bios.hpp
@@ -6,10 +6,12 @@
 #include <eosio/fixed_bytes.hpp>
 #include <eosio/privileged.hpp>
 #include <eosio/producer_schedule.hpp>
+#include "eosio.bios.ricardian.hpp"
 
-namespace eosiobios {
+namespace eosio_bios {
 
    using eosio::action_wrapper;
+   using eosio::authority;
    using eosio::check;
    using eosio::checksum256;
    using eosio::ignore;
@@ -17,63 +19,23 @@ namespace eosiobios {
    using eosio::permission_level;
    using eosio::public_key;
 
-   struct permission_level_weight {
-      permission_level  permission;
-      uint16_t          weight;
-
-      // explicit serialization macro is not necessary, used here only to improve compilation time
-      EOSLIB_SERIALIZE( permission_level_weight, (permission)(weight) )
+   struct abi_hash {
+      name              owner;
+      checksum256       hash;
+      uint64_t primary_key() const { return owner.value; }
    };
+   EOSIO_REFLECT(abi_hash, owner, hash)
 
-   struct key_weight {
-      eosio::public_key  key;
-      uint16_t           weight;
-
-      // explicit serialization macro is not necessary, used here only to improve compilation time
-      EOSLIB_SERIALIZE( key_weight, (key)(weight) )
-   };
-
-   struct wait_weight {
-      uint32_t           wait_sec;
-      uint16_t           weight;
-
-      // explicit serialization macro is not necessary, used here only to improve compilation time
-      EOSLIB_SERIALIZE( wait_weight, (wait_sec)(weight) )
-   };
-
-   struct authority {
-      uint32_t                              threshold = 0;
-      std::vector<key_weight>               keys;
-      std::vector<permission_level_weight>  accounts;
-      std::vector<wait_weight>              waits;
-
-      // explicit serialization macro is not necessary, used here only to improve compilation time
-      EOSLIB_SERIALIZE( authority, (threshold)(keys)(accounts)(waits) )
-   };
-
-   struct block_header {
-      uint32_t                                  timestamp;
-      name                                      producer;
-      uint16_t                                  confirmed = 0;
-      checksum256                               previous;
-      checksum256                               transaction_mroot;
-      checksum256                               action_mroot;
-      uint32_t                                  schedule_version = 0;
-      std::optional<eosio::producer_schedule>   new_producers;
-
-      // explicit serialization macro is not necessary, used here only to improve compilation time
-      EOSLIB_SERIALIZE(block_header, (timestamp)(producer)(confirmed)(previous)(transaction_mroot)(action_mroot)
-                                     (schedule_version)(new_producers))
-   };
+   typedef eosio::multi_index<"abihash"_n, abi_hash> abi_hash_table;
 
    /**
     * The `eosio.bios` is the first sample of system contract provided by `block.one` through the EOSIO platform. It is a minimalist system contract because it only supplies the actions that are absolutely critical to bootstrap a chain and nothing more. This allows for a chain agnostic approach to bootstrapping a chain.
     * 
     * Just like in the `eosio.system` sample contract implementation, there are a few actions which are not implemented at the contract level (`newaccount`, `updateauth`, `deleteauth`, `linkauth`, `unlinkauth`, `canceldelay`, `onerror`, `setabi`, `setcode`), they are just declared in the contract so they will show in the contract's ABI and users will be able to push those actions to the chain via the account holding the `eosio.system` contract, but the implementation is at the EOSIO core level. They are referred to as EOSIO native actions.
     */
-   class [[eosio::contract("eosio.bios")]] bios : public eosio::contract {
+   class contract : public eosio::contract {
       public:
-         using contract::contract;
+         using eosio::contract::contract;
          /**
           * New account action, called after a new account is created. This code enforces resource-limits rules
           * for new accounts as well as new account naming conventions.
@@ -86,7 +48,6 @@ namespace eosiobios {
           * therefore, this method will execute an inline buyram from receiver for newacnt in
           * an amount equal to the current new account creation fee.
           */
-         [[eosio::action]]
          void newaccount( name             creator,
                           name             name,
                           ignore<authority> owner,
@@ -99,7 +60,6 @@ namespace eosiobios {
           * @param parem - the parent of the permission which is updated,
           * @param aut - the json describing the permission authorization.
           */
-         [[eosio::action]]
          void updateauth(  ignore<name>  account,
                            ignore<name>  permission,
                            ignore<name>  parent,
@@ -111,7 +71,6 @@ namespace eosiobios {
           * @param account - the account for which the permission authorization is deleted,
           * @param permission - the permission name been deleted.
           */
-         [[eosio::action]]
          void deleteauth( ignore<name>  account,
                           ignore<name>  permission ) {}
 
@@ -130,7 +89,6 @@ namespace eosiobios {
           * @param type - the action to be linked,
           * @param requirement - the permission to be linked.
           */
-         [[eosio::action]]
          void linkauth(  ignore<name>    account,
                          ignore<name>    code,
                          ignore<name>    type,
@@ -143,7 +101,6 @@ namespace eosiobios {
           * @param code - the owner of the action to be unlinked,
           * @param type - the action to be unlinked.
           */
-         [[eosio::action]]
          void unlinkauth( ignore<name>  account,
                           ignore<name>  code,
                           ignore<name>  type ) {}
@@ -154,7 +111,6 @@ namespace eosiobios {
           * @param canceling_auth - the permission that authorizes this action,
           * @param trx_id - the deferred transaction id to be cancelled.
           */
-         [[eosio::action]]
          void canceldelay( ignore<permission_level> canceling_auth, ignore<checksum256> trx_id ) {}
 
          /**
@@ -165,7 +121,6 @@ namespace eosiobios {
           * @param vmversion - reserved, set it to zero.
           * @param code - the code content to be set, in the form of a blob binary..
           */
-         [[eosio::action]]
          void setcode( name account, uint8_t vmtype, uint8_t vmversion, const std::vector<char>& code ) {}
 
          /**
@@ -176,7 +131,6 @@ namespace eosiobios {
           * @param account - the name of the account to set the abi for
           * @param abi     - the abi hash represented as a vector of characters
           */
-         [[eosio::action]]
          void setabi( name account, const std::vector<char>& abi );
 
          /**
@@ -187,7 +141,6 @@ namespace eosiobios {
           * @param sender_id - the id for the deferred transaction chosen by the sender,
           * @param sent_trx - the deferred transaction that failed.
           */
-         [[eosio::action]]
          void onerror( ignore<uint128_t> sender_id, ignore<std::vector<char>> sent_trx );
 
          /**
@@ -195,7 +148,6 @@ namespace eosiobios {
           * @param account - the account to set the privileged status for.
           * @param is_priv - 0 for false, > 0 for true.
           */
-         [[eosio::action]]
          void setpriv( name account, uint8_t is_priv );
 
          /**
@@ -206,7 +158,6 @@ namespace eosiobios {
           * @param net_weight - fractionally proportionate net limit of available resources based on (weight / total_weight_of_all_accounts)
           * @param cpu_weight - fractionally proportionate cpu limit of available resources based on (weight / total_weight_of_all_accounts)
           */
-         [[eosio::action]]
          void setalimits( name account, int64_t ram_bytes, int64_t net_weight, int64_t cpu_weight );
 
          /**
@@ -217,7 +168,6 @@ namespace eosiobios {
           *
           * @param schedule - New list of active producers to set
           */
-         [[eosio::action]]
          void setprods( const std::vector<eosio::producer_authority>& schedule );
 
          /**
@@ -225,7 +175,6 @@ namespace eosiobios {
           *
           * @param params - New blockchain parameters to set
           */
-         [[eosio::action]]
          void setparams( const eosio::blockchain_parameters& params );
 
          /**
@@ -234,7 +183,6 @@ namespace eosiobios {
           *
           * @param from - the account name to authorize
           */
-         [[eosio::action]]
          void reqauth( name from );
 
          /**
@@ -242,7 +190,6 @@ namespace eosiobios {
           *
           * @param feature_digest - hash of the protocol feature to activate.
           */
-         [[eosio::action]]
          void activate( const eosio::checksum256& feature_digest );
 
          /**
@@ -250,33 +197,25 @@ namespace eosiobios {
           *
           * @param feature_digest - hash of the protocol feature to check for activation.
           */
-         [[eosio::action]]
          void reqactivated( const eosio::checksum256& feature_digest );
+   }; // class contract
 
-         struct [[eosio::table]] abi_hash {
-            name              owner;
-            checksum256       hash;
-            uint64_t primary_key()const { return owner.value; }
-
-            EOSLIB_SERIALIZE( abi_hash, (owner)(hash) )
-         };
-
-         typedef eosio::multi_index< "abihash"_n, abi_hash > abi_hash_table;
-
-         using newaccount_action = action_wrapper<"newaccount"_n, &bios::newaccount>;
-         using updateauth_action = action_wrapper<"updateauth"_n, &bios::updateauth>;
-         using deleteauth_action = action_wrapper<"deleteauth"_n, &bios::deleteauth>;
-         using linkauth_action = action_wrapper<"linkauth"_n, &bios::linkauth>;
-         using unlinkauth_action = action_wrapper<"unlinkauth"_n, &bios::unlinkauth>;
-         using canceldelay_action = action_wrapper<"canceldelay"_n, &bios::canceldelay>;
-         using setcode_action = action_wrapper<"setcode"_n, &bios::setcode>;
-         using setabi_action = action_wrapper<"setabi"_n, &bios::setabi>;
-         using setpriv_action = action_wrapper<"setpriv"_n, &bios::setpriv>;
-         using setalimits_action = action_wrapper<"setalimits"_n, &bios::setalimits>;
-         using setprods_action = action_wrapper<"setprods"_n, &bios::setprods>;
-         using setparams_action = action_wrapper<"setparams"_n, &bios::setparams>;
-         using reqauth_action = action_wrapper<"reqauth"_n, &bios::reqauth>;
-         using activate_action = action_wrapper<"activate"_n, &bios::activate>;
-         using reqactivated_action = action_wrapper<"reqactivated"_n, &bios::reqactivated>;
-   };
-}
+   EOSIO_ACTIONS(contract,
+                 "eosio"_n,
+                 action(newaccount, creator, name, owner, active, ricardian_contract(newaccount_ricardian)),
+                 action(updateauth, account, permission, parent, auth, ricardian_contract(updateauth_ricardian)),
+                 action(deleteauth, account, permission, ricardian_contract(deleteauth_ricardian)),
+                 action(linkauth, account, code, type, requirement, ricardian_contract(linkauth_ricardian)),
+                 action(unlinkauth, account, code, type, ricardian_contract(unlinkauth_ricardian)),
+                 action(canceldelay, canceling_auth, trx_id, ricardian_contract(canceldelay_ricardian)),
+                 action(setcode, account, vmtype, vmversion, code, ricardian_contract(setcode_ricardian)),
+                 action(setabi, account, abi, ricardian_contract(setabi_ricardian)),
+                 action(onerror, sender_id, sent_trx),
+                 action(setpriv, account, is_priv, ricardian_contract(setpriv_ricardian)),
+                 action(setalimits, account, ram_bytes, net_weight, cpu_weight, ricardian_contract(setalimits_ricardian)),
+                 action(setprods, schedule, ricardian_contract(setprods_ricardian)),
+                 action(setparams, params, ricardian_contract(setparams_ricardian)),
+                 action(reqauth, from, ricardian_contract(reqauth_ricardian)),
+                 action(activate, feature_digest, ricardian_contract(activate_ricardian)),
+                 action(reqactivated, feature_digest, ricardian_contract(reqactivated_ricardian)))
+} // namespace eosio_bios
--- contracts/eosio.bios/src/eosio.bios.cpp
+++ contracts/eosio.bios/src/eosio.bios.cpp
@@ -1,8 +1,8 @@
 #include <eosio.bios/eosio.bios.hpp>
 
-namespace eosiobios {
+namespace eosio_bios {
 
-void bios::setabi( name account, const std::vector<char>& abi ) {
+void contract::setabi( name account, const std::vector<char>& abi ) {
    abi_hash_table table(get_self(), get_self().value);
    auto itr = table.find( account.value );
    if( itr == table.end() ) {
@@ -17,41 +17,47 @@ void bios::setabi( name account, const std::vector<char>& abi ) {
    }
 }
 
-void bios::onerror( ignore<uint128_t>, ignore<std::vector<char>> ) {
+void contract::onerror( ignore<uint128_t>, ignore<std::vector<char>> ) {
    check( false, "the onerror action cannot be called directly" );
 }
 
-void bios::setpriv( name account, uint8_t is_priv ) {
+void contract::setpriv( name account, uint8_t is_priv ) {
    require_auth( get_self() );
    set_privileged( account, is_priv );
 }
 
-void bios::setalimits( name account, int64_t ram_bytes, int64_t net_weight, int64_t cpu_weight ) {
+void contract::setalimits( name account, int64_t ram_bytes, int64_t net_weight, int64_t cpu_weight ) {
    require_auth( get_self() );
    set_resource_limits( account, ram_bytes, net_weight, cpu_weight );
 }
 
-void bios::setprods( const std::vector<eosio::producer_authority>& schedule ) {
+void contract::setprods( const std::vector<eosio::producer_authority>& schedule ) {
    require_auth( get_self() );
    set_proposed_producers( schedule );
 }
 
-void bios::setparams( const eosio::blockchain_parameters& params ) {
+void contract::setparams( const eosio::blockchain_parameters& params ) {
    require_auth( get_self() );
    set_blockchain_parameters( params );
 }
 
-void bios::reqauth( name from ) {
+void contract::reqauth( name from ) {
    require_auth( from );
 }
 
-void bios::activate( const eosio::checksum256& feature_digest ) {
+void contract::activate( const eosio::checksum256& feature_digest ) {
    require_auth( get_self() );
    preactivate_feature( feature_digest );
 }
 
-void bios::reqactivated( const eosio::checksum256& feature_digest ) {
+void contract::reqactivated( const eosio::checksum256& feature_digest ) {
    check( is_feature_activated( feature_digest ), "protocol feature is not activated" );
 }
 
-}
+} // namespace eosio_bios
+
+EOSIO_ACTION_DISPATCHER(eosio_bios::actions)
+EOSIO_ABIGEN(
+   variant("block_signing_authority", eosio::block_signing_authority),
+   actions(eosio_bios::actions),
+   table("abihash"_n, eosio_bios::abi_hash))
