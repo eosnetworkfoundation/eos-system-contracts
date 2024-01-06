96509e767f2647378ea4a17769601690592bc87b
Denis Carriere <carriere.denis@gmail.com>
2024-01-06 16:10:08 -0500

    update tests

--- tests/eosio.system_tests.cpp
+++ tests/eosio.system_tests.cpp
@@ -4123,32 +4123,6 @@ BOOST_FIXTURE_TEST_CASE( buy_sell_small_rex, eosio_system_tester ) try {
 
 } FC_LOG_AND_RETHROW()
 
-
-// RAM transfer
-BOOST_FIXTURE_TEST_CASE( ram_transfer, eosio_system_tester ) try {
-   const std::vector<account_name> accounts = { "alice"_n, "bob"_n };
-   create_accounts_with_resources( accounts );
-   const account_name alice = accounts[0];
-   const account_name bob = accounts[1];
-
-   transfer( config::system_account_name, alice, core_sym::from_string("100.0000"), config::system_account_name );
-   transfer( config::system_account_name, bob, core_sym::from_string("100.0000"), config::system_account_name );
-   BOOST_REQUIRE_EQUAL( success(), buyrambytes( alice, alice, 10000 ) );
-   BOOST_REQUIRE_EQUAL( success(), buyrambytes( bob, bob, 10000 ) );
-
-   const uint64_t alice_before = get_total_stake( alice )["ram_bytes"].as_uint64();
-   const uint64_t bob_before = get_total_stake( bob )["ram_bytes"].as_uint64();
-
-   BOOST_REQUIRE_EQUAL( success(), ramtransfer( alice, bob, 1000 ) );
-
-   const uint64_t alice_after = get_total_stake( alice )["ram_bytes"].as_uint64();
-   const uint64_t bob_after = get_total_stake( bob )["ram_bytes"].as_uint64();
-
-   BOOST_REQUIRE_EQUAL( alice_before - 1000, alice_after );
-   BOOST_REQUIRE_EQUAL( bob_before + 1000, bob_after );
-
-} FC_LOG_AND_RETHROW()
-
 BOOST_FIXTURE_TEST_CASE( unstake_buy_rex, eosio_system_tester, * boost::unit_test::tolerance(1e-10) ) try {
 
    const int64_t ratio        = 10000;
