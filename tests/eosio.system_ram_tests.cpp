4e1eed950dd0791af36f9a560163ee7ed361e96e
Denis Carriere <carriere.denis@gmail.com>
2024-01-24 12:23:37 -0500

    add null account burn tests

--- tests/eosio.system_ram_tests.cpp
+++ tests/eosio.system_ram_tests.cpp
@@ -20,11 +20,13 @@ BOOST_FIXTURE_TEST_CASE( ram_transfer, eosio_system_tester ) try {
    create_accounts_with_resources( accounts );
    const account_name alice = accounts[0];
    const account_name bob = accounts[1];
+   const account_name null_account = "eosio.null"_n;
 
    transfer( config::system_account_name, alice, core_sym::from_string("100.0000"), config::system_account_name );
    transfer( config::system_account_name, bob, core_sym::from_string("100.0000"), config::system_account_name );
    BOOST_REQUIRE_EQUAL( success(), buyrambytes( alice, alice, 10000 ) );
    BOOST_REQUIRE_EQUAL( success(), buyrambytes( bob, bob, 10000 ) );
+   BOOST_REQUIRE_EQUAL( success(), buyrambytes( alice, null_account, 10000 ) );
 
    const uint64_t alice_before = get_total_stake( alice )["ram_bytes"].as_uint64();
    const uint64_t bob_before = get_total_stake( bob )["ram_bytes"].as_uint64();
@@ -38,9 +40,13 @@ BOOST_FIXTURE_TEST_CASE( ram_transfer, eosio_system_tester ) try {
    BOOST_REQUIRE_EQUAL( bob_before + 1000, bob_after );
 
    // RAM burn
+   const uint64_t null_before_burn = get_total_stake( null_account )["ram_bytes"].as_uint64();
+   const uint64_t alice_before_burn = get_total_stake( alice )["ram_bytes"].as_uint64();
    BOOST_REQUIRE_EQUAL( success(), ramburn( alice, 3000, "burn RAM memo" ) );
    const uint64_t alice_after_burn = get_total_stake( alice )["ram_bytes"].as_uint64();
-   BOOST_REQUIRE_EQUAL( alice_before - 4000, alice_after_burn );
+   const uint64_t null_after_burn = get_total_stake( null_account )["ram_bytes"].as_uint64();
+   BOOST_REQUIRE_EQUAL( alice_before_burn - 3000, alice_after_burn );
+   BOOST_REQUIRE_EQUAL( null_before_burn + 3000, null_after_burn );
 
 } FC_LOG_AND_RETHROW()
 
