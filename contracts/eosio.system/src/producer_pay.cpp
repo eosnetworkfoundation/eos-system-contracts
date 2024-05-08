5c0256c806f8f343b588a24cd79d7bb27116a72a
Denis Carriere <carriere.denis@gmail.com>
2024-05-08 11:18:00 +0200

    rename to "unallocated bucket"

--- contracts/eosio.system/src/producer_pay.cpp
+++ contracts/eosio.system/src/producer_pay.cpp
@@ -113,7 +113,7 @@ namespace eosiosystem {
             {
                token::transfer_action transfer_act{ token_account, { {get_self(), active_permission} } };
                if( to_savings > 0 ) {
-                  transfer_act.send( get_self(), saving_account, asset(to_savings, core_symbol()), "unallocated inflation" );
+                  transfer_act.send( get_self(), saving_account, asset(to_savings, core_symbol()), "unallocated bucket" );
                }
                if( to_per_block_pay > 0 ) {
                   transfer_act.send( get_self(), bpay_account, asset(to_per_block_pay, core_symbol()), "fund per-block bucket" );
